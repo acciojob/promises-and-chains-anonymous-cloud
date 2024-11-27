a66d034231fbc444fe71577b63684a5f3d6a250a
abhishek <57187026+anonymous-cloud@users.noreply.github.com>
2024-11-27 14:17:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+document.getElementById("ageForm").addEventListner("click",function(event){
+
+	 event.preventDefault();
+
+ let age = document.querySelectorById("age").value.trim;
+let name  = document.querySelectorById("name").value.trim;
+
+	if(!name|| !age ){
+		alert(""name and age cant be empty")
+			return;
+	}
+
+	const ageNumber>18 = parseInt(age);
+	let promise = new promise(resolve,reject){
+
+		setTimeOut(() =>{
+			if(ageNumber>18){
+				alert(`welcome ${name} . You can vote`)
+			}else{
+				alert(`Oh sorry ${name}. You aren't old enough.`)
+			}
+		},4000);
+
+});
+promise
+ .then(message => alert(message))
+        .catch(errorMessage => alert(errorMessage));
+
